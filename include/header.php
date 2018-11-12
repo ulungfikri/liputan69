bf348f0f62473d7ce5276a57849d56ed97e451b4
ulungfikri <ulung.fikri@gmail.com>
2018-11-12 13:01:11 +0700

    add sso2

--- include/header.php
+++ include/header.php
@@ -1,8 +1,4 @@
 <div class="login d-flex">
-<!-- 	<a href="" onclick="mncdigLogin('WAwLDt8cPUQ85204')">Login</a>
-	<a href="" onclick="mncdigRegister()">Register</a>
-	<a href="" onclick="mncdigLogout()">Logout</a> -->
-
 	<button href="" onclick="mncdigRegister()" class="btn btn-danger"> Sign up</button>&nbsp;
 
 	<button href="" onclick="mncdigLogin('WAwLDt8cPUQ85204')" class="btn btn-danger"> Log in</button>&nbsp;
