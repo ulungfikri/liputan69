cfca2e93eae2aae2d112f09775220293c8fded90
ulungfikri <ulung.fikri@gmail.com>
2018-11-14 12:59:15 +0700

    update nav

--- bak/nav_bak.php
+++ bak/nav_bak.php
@@ -3,6 +3,12 @@
 
 include "koneksi.php";
 
+// $query = "SELECT rowid, * FROM BERITA";
+
+$query = "SELECT a.ID, a.TITLE, a.AUTHOR, a.IMAGE, a.CONTENT1, a.CONTENT2, a.VIEWS, a.IDKAT, b.IDKAT, b.NAMA FROM BERITA a JOIN KATEGORI b ON a.IDKAT=b.IDKAT WHERE a.IDKAT='$_GET[kat]' ORDER BY a.ID DESC";
+
+$result = $db->query($query);
+
 ?>
 
 <div class="classynav">
